229eaeac5795fd6aa6c7cd41e958f30d31a25fc4
Siddhesh Gaykar <64803476+Siddhya007@users.noreply.github.com>
2020-07-16 17:17:50 +0530

    Add files via upload

---
+++ precache-manifest.9e374fec542acf537ff7fd52e6a54c4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8bdf715a68782f464f5b1c57cdf01f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa55115e512c3fad86f4",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "9766392f5c555b4d7d35",
+    "url": "/static/css/main.a18cb4d1.chunk.css"
+  },
+  {
+    "revision": "fa55115e512c3fad86f4",
+    "url": "/static/js/2.0670bfb8.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/static/js/2.0670bfb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9766392f5c555b4d7d35",
+    "url": "/static/js/main.556c869d.chunk.js"
+  },
+  {
+    "revision": "afd9fccc04eb1a18eb43",
+    "url": "/static/js/runtime-main.0c7a2f62.js"
+  },
+  {
+    "revision": "68838afd08a6a0293857b44c23c661b9",
+    "url": "/static/media/bg2.68838afd.jpg"
+  }
+]);
